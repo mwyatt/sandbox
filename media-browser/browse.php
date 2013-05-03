59f5c5267983114aa452bdada86321e4b6a80a11
mwyatt <martin.wyatt@gmail.com>
2013-05-03 17:18:54 +0100

    media browser improvements

--- media-browser/browse.php
+++ media-browser/browse.php
@@ -2,18 +2,18 @@
 define('BASE_PATH', (string) (__DIR__ . '/'));
 
 /**
-* 
+* browses directory and retrieves file information
 */
 class Browse
 {
 	
 
-	public $directory = 'media/';
-
-
-	public function getDirectory($directory) {
-		$directory = $directory;
+	public function getDirectory($directory = 'media/') {
 		$handler = glob(BASE_PATH . $directory . '*', GLOB_MARK);
+		$items = array(
+			'directory' => false
+			, 'file' => false
+		);
 		foreach ($handler as $key => $handle) {
 			$fileInfo = pathinfo($handle);
 			if (is_dir($handle)) {
@@ -24,9 +24,6 @@ class Browse
 			}
 		}
 		echo json_encode($items);
-		// echo '<pre>';
-		// print_r($items);
-		// echo '</pre>';
 	}
 
 
@@ -36,20 +33,50 @@ class Browse
 		}
 		$pathParts = explode('/media', $path);
 		$fileInfo = pathinfo($path);
-		$fileInfo['guid'] = $this->directory . ltrim($pathParts[1], '/');
-		// echo '<pre>';
-		// print_r($fileInfo);
-		// echo '</pre>';
+		$fileInfo['guid'] = 'media/' . ltrim($pathParts[1], '/');
 		echo json_encode($fileInfo);
 	}
 
+	public function createDirectory($path) {
+		if (!file_exists($path)) {
+		    if (mkdir($path)) {
+				echo json_encode($path);
+		    }
+		}
+	}
+
+
+	public function deleteFile($path) {
+		if (file_exists($path)) {
+		    if (unlink($path)) {
+				echo json_encode($path);
+		    }
+		}
+	}
+
+	public function deleteDirectory($path) {
+		if (is_dir($path)) {
+			if (rmdir($path)) {
+				echo json_encode($path);
+			}
+		}
+	}
+
+	public function upload() {
+		foreach ($_FILES["images"]["error"] as $key => $error) {
+		    if ($error == UPLOAD_ERR_OK) {
+		        $name = $_FILES["images"]["name"][$key];
+		        move_uploaded_file( $_FILES["images"]["tmp_name"][$key], $_GET['directory'] . $_FILES['images']['name'][$key]);
+		    }
+		}
+	}
 
 }
 
 $browse = new Browse();
-$browse->$_GET['o']['method']($_GET['o']['action']);
 
-// if (array_key_exists('method', $_GET)) {
-// 	if (method_exists($browse, $_GET['method'])) {
-// 	}
-// }
+if (! empty($_FILES)) {
+	$browse->upload();
+} else {
+	$browse->$_GET['o']['method']($_GET['o']['action']);
+}
