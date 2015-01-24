3c6bad3046ca6f599eb38077a2bdce90005633ed
mwyatt <martin.wyatt@gmail.com>
2015-01-24 07:49:24 +0000

    require js

--- requirejs/js/main.js
+++ requirejs/js/main.js
@@ -1,12 +1,8 @@
-define(function (require) {
-    // Load any app-specific modules
-    // with a relative require call,
-    // like:
-    var messages = require('./module');
 
-    // Load library/vendor modules using
-    // full IDs, like:
-    // var print = require('print');
-console.log(messages);
-    // print(messages.getHello());
+
+/**
+ * description
+ */
+require(['vendor/jquery'], function ($) {
+    console.log($('body'));
 });
