267a8b7a5e374dc9f6ccaa6ab1f99cdde4b093ad
mwyatt <martin.wyatt@gmail.com>
2015-05-22 17:28:47 +0100

    gulp

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -1,6 +1,9 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
+var imagemin = require('gulp-imagemin');
+// var pngquant = require('imagemin-pngquant');
+var browserSync = require('browser-sync').create();
 
 gulp.task('sass', function () {
   gulp.src('./sass/**/*.scss')
@@ -10,4 +13,23 @@ gulp.task('sass', function () {
         cascade: false
     }))
     .pipe(gulp.dest('./asset'));
+});
+
+gulp.task('image', function () {
+    return gulp.src('image/*')
+        .pipe(imagemin({
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant()]
+        }))
+        .pipe(gulp.dest('./asset'));
+});
+
+// Static server
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
 });
---
+++ gulp/index.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Flexbox</title>
+	<link rel="stylesheet" href="asset/common.css">
+</head>
+<body>
+	<div class="canvas">
+		<div class="prop"></div>
+	</div>
+	<div class="container">
+		
+<?php foreach ([1, 2, 3] as $item): ?>
+	
+		<div class="item">item</div>
+
+<?php endforeach ?>
+
+	</div>
+</body>
+</html>
