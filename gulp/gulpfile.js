f1dc28cb86e2807da96a7f0311316c9c191b7e8f
mwyatt <martin.wyatt@gmail.com>
2015-05-28 14:00:57 +0100

    gulp postcss

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -1,51 +1,41 @@
 var gulp = require('gulp');
-
-// post css processor
 var postcss = require('gulp-postcss');
+var csswring = require('csswring');
+var autoprefixer = require('autoprefixer-core');
+var customProperties = require('postcss-custom-properties');
+var atImport = require('postcss-import');
+var mixins = require('postcss-mixins');
+var colorFunction = require("postcss-color-function")
+var uglify = require('gulp-uglify');
 
-  // css minify
-  var csswring = require('csswring');
-
-  // transition:
-  var autoprefixer = require('autoprefixer-core');
-
-  // --foo-bar
-  var cssvariables = require('postcss-css-variables');
-
-  // @import
-  var atImport = require('postcss-import');
-
-  var assets  = require('postcss-assets');
-
-
-var postcssProcessors = [
-  atImport,
-  cssvariables,
-  autoprefixer({browsers: ['last 1 version']}),
-  assets({
-    relativeTo: 'asset/',
-    loadPaths: ['asset/']
-  })
+var css = ['./css/**/*.css', '!./css/**/_*.css'];
+var processes = [
+  atImport(),
+  customProperties(),
+  colorFunction(),
+  mixins(),
+  autoprefixer({browsers: ['last 1 version']})
 ];
 
-// css bundle files
-var css = ['./css/**/*.css', '!./css/**/_*.css'];
-
-function doPostCss () {
-  return gulp.src(css)
-    .pipe(postcss(postcssProcessors))
-    .pipe(gulp.dest('./asset'))
+var action = {
+  postcss: function() {
+    return gulp.src(css)
+      .pipe(postcss(processes))
+      .pipe(gulp.dest('asset'));
+  }
 }
 
-// css build
 gulp.task('css', function () {
-  return doPostCss();
+  return action.postcss();
 });
 
-// css build minify
 gulp.task('cssm', function () {
-  postcssProcessors.push(csswring);
-  return doPostCss();
+  process.push(csswring);
+  return action.postcss();
 });
 
-// js minify
+gulp.task('jsm', function() {
+  return gulp.src('asset/**/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('asset'));
+});
