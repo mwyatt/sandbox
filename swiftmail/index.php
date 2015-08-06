2c820f2121e25367b8c91b8f38dffda38beb22a1
mwyatt <martin.wyatt@gmail.com>
2015-08-06 17:34:34 +0100

    sandbox mail test

--- swiftmail/index.php
+++ swiftmail/index.php
@@ -2,15 +2,22 @@
 
 include 'vendor/autoload.php';
 
-// Create the message
+$transport = Swift_SmtpTransport::newInstance('smtp.gmail.com', 465, 'ssl')
+  ->setUsername('martin.wyatt@gmail.com')
+  ->setPassword(include 'config.php');
+$mailer = Swift_Mailer::newInstance($transport);
 $message = Swift_Message::newInstance()
   ->setSubject('Your subject')
   ->setFrom(['sales@audiovisualonline.co.uk' => 'Sales Man'])
   ->setTo(['martin.wyatt@gmail.com' => 'Martin Wyatt'])
   ->setBody('<h1>Here is the message itself</h1>', 'text/html');
+try {
+  $result = $mailer->send($message);
+  var_dump($result);
+} catch (Swift_TransportException $e) {
+  var_dump($e->getMessage());
+}
 
-  echo '<pre>';
-  print_r($message);
-  echo '</pre>';
-  exit;
-  
+// do stuff with swiftmessage
+// construct to init the transport and mailer
+$mail->message->setSubject
